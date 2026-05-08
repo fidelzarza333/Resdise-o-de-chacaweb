ef116a35b6d341c4dee4ba1a087ddc73ac2642d9
valentintomassanchezdibella-creator <valentintomassanchezdibella@gmail.com>
2026-05-08 11:54:18 -0300

    8/5
    mejorado responsividad en chaca.html
    
    - Todas las subpaginas tienen descripción completada
    
    - Integrado al principio los fonts 'Titulo', 'Texto', 'Texto-boldItalic'
    
    optimizacion del script
    
    añadio una seccion nueva en especialidades

--- pagina/script.js
+++ pagina/script.js
@@ -201,41 +201,33 @@ let index = 2;
 let imageWidth = 500;
 let contador = 0;
 
-function updateCarousel(){
+function updateCarousel(contador){
   wrappers.forEach(w => w.classList.remove("active"));
   wrappers[index].classList.add("active");
-
-
-  
-  console.log(index)
+  WrapperContainer.style.transform = `translateX(${-(imageWidth*contador)}px)`;
 }
 
 btnNext.addEventListener("click", () =>{
   index ++;
   contador ++;
-  WrapperContainer.style.transform = `translateX(${-(500*contador)}px)`;
-
+  
   if(index > wrappers.length-1){
     index = 0;
     contador = -2;
-    WrapperContainer.style.transform = `translateX(${-(500*contador)}px)`;
   }
-
-  updateCarousel();
+  
+  updateCarousel(contador);
 })
 
 btnPrev.addEventListener("click", () =>{
   index --;
   contador --;
-  WrapperContainer.style.transform = `translateX(${-(500*contador)}px)`;
 
   if(index < 0){
     index = wrappers.length-1;
     contador = 2;
-    WrapperContainer.style.transform = `translateX(${-(500*contador)}px)`;
   }
-
   
-  updateCarousel();
+  updateCarousel(contador);
 
 })
